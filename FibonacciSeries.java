03dca1412ced91007f71ba2e00230e05596fe6e6
Yuvasri. S <yuvasris807@gmail.com>
2026-05-06 10:56:14 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
-
-public class FibonacciSeries {
+public class fibonacciNumber {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Enter the number of terms: ");
-        int n = input.nextInt();
-        
-        int firstTerm = 0, secondTerm = 1;
-        
-        System.out.println("Fibonacci Series up to " + n + " terms:");
-
-        for (int i = 1; i <= n; ++i) {
-            System.out.print(firstTerm + ", ");
-
-            // Calculate the next term
-            int nextTerm = firstTerm + secondTerm;
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        int a = 0, b = 1;
+        for (int i = 1; i <=n; i++) {
+            System.out.println(a+" ");
+            int c = a+b;
+            a = b;
+            b = c;
+            
         }
-        
-        input.close();
+
     }
+    
 }
+
