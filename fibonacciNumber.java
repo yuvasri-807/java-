d36f49a8a1c0f34a39b4ef1a34b6bc7c46159986
Yuvasri. S <yuvasris807@gmail.com>
2026-05-06 11:00:31 +0530

    Add files via upload

---
+++ fibonacciNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class fibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        int a = 0, b = 1;
+        for (int i = 1; i <=n; i++) {
+            System.out.println(a+" ");
+            int c = a+b;
+            a = b;
+            b = c;
+            
+        }
+
+    }
+    
+}
